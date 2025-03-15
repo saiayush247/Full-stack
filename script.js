6b5124983d16e444c66f2cf462d93f2dca8033fc
Sai Ayush Pattnaik <88043107+saiayush247@users.noreply.github.com>
2025-03-15 07:52:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskList = document.getElementById("taskList");
+
+    if (taskInput.value === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.innerHTML = `${taskInput.value} <button class="delete-btn" onclick="deleteTask(this)">X</button>`;
+
+    taskList.appendChild(li);
+    taskInput.value = "";
+}
+
+function deleteTask(button) {
+    button.parentElement.remove();
+}
